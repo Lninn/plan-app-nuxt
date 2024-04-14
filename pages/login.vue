e8612586b6b91d75b243ae25625368b19940f920
Lninn <thl384458870@outlook.com>
2024-04-14 12:01:50 +0800

    登录

--- pages/login.vue
+++ pages/login.vue
@@ -1,31 +1,126 @@
 <script setup lang="ts">
+import type { FormInstance, FormItemRule, FormRules } from 'element-plus'
+
 const supabase = useSupabaseClient()
-const email = ref('')
+
+const loginLoading = ref(false)
+const formSize = ref('default' as const)
+const ruleFormRef = ref<FormInstance>()
+interface RuleForm {
+  email: string
+}
+const ruleForm = reactive<RuleForm>({
+  email: '',
+})
+
+const emailJumpOpen = ref(false)
+
+const emailValidator: FormItemRule['validator'] = (
+  _, value, callback,
+) => {
+  const EMAIL_REGEXP = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+
+  if (value === '') {
+    callback(new Error('请输入邮箱地址'))
+  }
+  else if (EMAIL_REGEXP.test(value) === false) {
+    callback(new Error('请输入正确的邮箱地址'))
+  }
+  else {
+    callback()
+  }
+}
+const rules = reactive<FormRules<RuleForm>>({
+  email: [
+    {
+      required: true,
+      trigger: 'change',
+      validator: emailValidator,
+    },
+  ],
+})
 
 const signInWithOtp = async () => {
-  if (!email.value) {
+  if (!ruleForm.email) {
     alert('Please enter an email')
     return
   }
 
-  const { error } = await supabase.auth.signInWithOtp({
-    email: email.value,
-    options: {
-      emailRedirectTo: 'http://localhost:3000/confirm',
-    },
+  try {
+    loginLoading.value = true
+    const { error } = await supabase.auth.signInWithOtp({
+      email: ruleForm.email,
+      options: {
+        emailRedirectTo: 'http://localhost:3000/confirm',
+      },
+    })
+    if (!error) {
+      emailJumpOpen.value = true
+    }
+  }
+  catch (error) {
+  //
+  }
+  finally {
+    loginLoading.value = false
+  }
+}
+
+const submitForm = async (formEl: FormInstance | undefined) => {
+  if (!formEl) return
+  await formEl.validate((valid) => {
+    if (valid) {
+      signInWithOtp()
+    }
   })
-  if (error) console.log(error)
+}
+
+const resetForm = (formEl: FormInstance | undefined) => {
+  if (!formEl) return
+  formEl.resetFields()
 }
 </script>
 
 <template>
   <div>
-    <button @click="signInWithOtp">
-      Sign In with E-Mail
-    </button>
-    <input
-      v-model="email"
-      type="email"
+    <el-form
+      v-if="!emailJumpOpen"
+      ref="ruleFormRef"
+      style="max-width: 600px;margin: 0 auto;"
+      :model="ruleForm"
+      :rules="rules"
+      label-width="auto"
+      class="demo-ruleForm"
+      :size="formSize"
+      status-icon
     >
+      <el-form-item
+        label="邮件地址"
+        prop="email"
+      >
+        <el-input
+          v-model="ruleForm.email"
+          type="email"
+          autocomplete="on"
+        />
+      </el-form-item>
+      <el-form-item>
+        <el-button
+          type="primary"
+          :loading="loginLoading"
+          @click="submitForm(ruleFormRef)"
+        >
+          登录
+        </el-button>
+        <el-button @click="resetForm(ruleFormRef)">
+          重置
+        </el-button>
+      </el-form-item>
+    </el-form>
+    <div v-else>
+      <el-button>
+        请检查邮箱地址
+      </el-button>
+    </div>
   </div>
 </template>
